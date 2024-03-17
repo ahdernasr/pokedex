fa2d1215644ef761fbe343481001e7f7660ca737
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2024-03-17 15:02:50 -0400

    feat: animations and next, prev buttons

--- client/src/pages/PokemonView.jsx
+++ client/src/pages/PokemonView.jsx
@@ -2,9 +2,12 @@ import { ProgressSpinner } from "primereact/progressspinner";
 import { GET_POKEMON_BY_ID } from "../queries/pokemonQueries";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
 
 const PokemonView = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const { loading, error, data } = useQuery(GET_POKEMON_BY_ID, {
     variables: { id: id },
@@ -39,16 +42,40 @@ const PokemonView = () => {
       {!loading && (
         <>
           <div className="viewSectionContainer">
-            <div className="viewSection">
+            <div
+              className="fadeInAnimation viewSection"
+              style={{ animationDuration: "0.35s", animationDelay: "0.05s" }}
+            >
               <div className="viewHeader">{data.pokemon.name}</div>
               <img
                 src={`https://img.pokemondb.net/artwork/${data.pokemon.name.toLowerCase()}.jpg`}
                 alt={data.pokemon.name}
                 className="viewImage"
               />
+              <div className="viewButtonContainer">
+                <Button
+                  className="viewButton"
+                  onClick={() => {
+                    navigate(`/pokemon/${id > 1 ? parseInt(id) - 1 : id}`);
+                  }}
+                >
+                  Prev
+                </Button>
+                <Button
+                  className="viewButton"
+                  onClick={() => {
+                    navigate(`/pokemon/${id <= 810 ? parseInt(id) + 1 : id}`);
+                  }}
+                >
+                  Next
+                </Button>
+              </div>
             </div>
             <div className="viewSection">
-              <div className="stat">
+              <div
+                className="stat fadeInLeftAnimation"
+                style={{ animationDuration: "1s", animationDelay: "0.05s" }}
+              >
                 <p className="stat-header">Types:</p>
                 {data.pokemon.type.map((type) => (
                   <span
@@ -63,37 +90,58 @@ const PokemonView = () => {
                   </span>
                 ))}
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.15s" }}
+              >
                 <p className="stat-header">HP</p>
                 <progress value={data.pokemon.base.HP} max="255" />
                 <p className="stat-value">{data.pokemon.base.HP}</p>
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.25s" }}
+              >
                 <p className="stat-header">Attack</p>
                 <progress value={data.pokemon.base.Attack} max="190" />
                 <p className="stat-value">{data.pokemon.base.Attack}</p>
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.35s" }}
+              >
                 <p className="stat-header">Defense</p>
                 <progress value={data.pokemon.base.Defense} max="250" />
                 <p className="stat-value">{data.pokemon.base.Defense}</p>
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.45s" }}
+              >
                 <p className="stat-header">Sp. Attack</p>
                 <progress value={data.pokemon.base.SpAttack} max="194" />
                 <p className="stat-value">{data.pokemon.base.SpAttack}</p>
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.55s" }}
+              >
                 <p className="stat-header">Sp. Defense</p>
                 <progress value={data.pokemon.base.SpDefense} max="250" />
                 <p className="stat-value">{data.pokemon.base.SpDefense}</p>
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.65s" }}
+              >
                 <p className="stat-header">Speed</p>
                 <progress value={data.pokemon.base.Speed} max="250" />
                 <p className="stat-value">{data.pokemon.base.Speed}</p>
               </div>
-              <div className="stat">
+              <div
+                className="stat fadeInAnimation"
+                style={{ animationDuration: "0.35s", animationDelay: "0.75s" }}
+              >
                 <p className="stat-header">Total</p>
                 <progress
                   value={
@@ -106,14 +154,14 @@ const PokemonView = () => {
                   }
                   max="780"
                 />
-                <p className="stat-value">{
-                    data.pokemon.base.Speed +
+                <p className="stat-value">
+                  {data.pokemon.base.Speed +
                     data.pokemon.base.SpDefense +
                     data.pokemon.base.SpAttack +
                     data.pokemon.base.Defense +
                     data.pokemon.base.Attack +
-                    data.pokemon.base.HP
-                  }</p>
+                    data.pokemon.base.HP}
+                </p>
               </div>
             </div>
           </div>
